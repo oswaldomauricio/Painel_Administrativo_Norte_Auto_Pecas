5f174a44c43b1f6ed1f4872a43c5e7d263a5e539
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2024-11-27 09:44:47 -0400

    feat: controller de login

---
+++ Controllers/LoginController.py
@@ -0,0 +1,29 @@
+import sys
+import os
+import pandas as pd
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from services.dataBase_ORACLE import get_connection
+
+class Login:
+    def __init__(self,  name, password):
+        self.name = name
+        self.password = password
+
+    def execute_query(self):
+        conn = get_connection()
+        
+        dados = pd.read_sql("""
+            SELECT * FROM ANALYTICS.USERS U 
+            WHERE NAME = :name AND PASSWORD = :password
+        """, conn, params={"name": self.name, "password": self.password})
+
+        conn.close()
+
+        return dados
+    
+# login = Login(name="oswaldo", password="44844")
+# dados = login.execute_query()
+
+# print(dados)
